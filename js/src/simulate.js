965d385c9cbff8eb044f678bbdcf97c647e04493
Shevelev.MS <Shevelev-MS@dns-shop.ru>
2020-12-16 23:11:15 +1000

    реализация одного файла

--- js/src/simulate.js
+++ js/src/simulate.js
@@ -1,5 +1,6 @@
 let iteration = 0;
 let simulateObject = {
+    insertedFile: "",
     spinPositions: [],
     spinDirections: [],
     simulateNew: () => {
@@ -29,12 +30,9 @@ let simulateObject = {
     },
 
     downloadFile: () => {
-        const dataPositions = [simulateObject.spinPositions];
-        const dataDirections = [simulateObject.spinDirections];
-        var bbp = new Blob(dataPositions, { type: 'text/plain' });
-        var bbd = new Blob(dataDirections, { type: 'text/plain' });
-        saveAs(bbp, "positions.txt");
-        saveAs(bbd, "directions.txt");
+        const spinsStrings = parseToFile(simulateObject.spinPositions, simulateObject.spinDirections);
+        var bbp = new Blob(spinsStrings, { type: 'text/plain' });
+        saveAs(bbp, "spins.txt");
     },
 
     createwebglspins: (n) => {
@@ -49,5 +47,45 @@ let simulateObject = {
                 [WebGLSpins.renderers.COORDINATESYSTEM, [0.0, 0.2, 0.2, 0.2]]
             ]
         });
+    }
+}
+
+function parseToFile(spinPositions, spinDirections) {
+    const spinsStrings = [];
+    console.log(spinPositions)
+    for (let index = 0; index < spinPositions.length; index += 3) {
+        let spinPosition = spinPositions.slice(index, index + 3);
+        let spinDirection = spinDirections.slice(index, index + 3);
+
+        let pos = spinPosition.map(el => `${el} \t`);
+        let dir = spinDirection.map(el => `${el} \t`);
+        let result = "";
+        pos.forEach(p => result += p);
+        dir.forEach(p => result += p);
+        // чтобы в последней строке не добавлялся снос
+        if (index != spinPositions.length - 3)
+            result += "\n";
+        spinsStrings.push(result);
+    }
+    console.log(spinsStrings)
+    return spinsStrings;
+}
+
+function parseToArrays(text, simulateObject) {
+    const spinsStrings = text.split('\n');
+    simulateObject.spinPositions = [];
+    simulateObject.spinDirections = [];
+
+    for (let index = 0; index < spinsStrings.length; index += 1) {
+        let str = spinsStrings[index].split('\t');
+        let positions = str.slice(0, 3);
+        let directions = str.slice(3, 6);
+
+        for (let index = 0; index < 3; index++) {
+            const position = parseFloat(positions[index]);
+            const direction = parseFloat(directions[index]);
+            simulateObject.spinPositions.push(position);
+            simulateObject.spinDirections.push(direction);
+        }
     }
 }
